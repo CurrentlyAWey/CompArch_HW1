586ab2a6268a50f37c1fff8f480cf04e40038dde
PandaStrawberry <wey@cooper.edu>
2016-02-24 12:45:52 -0500

    compiling stuff

--- processorcore.v
+++ processorcore.v
@@ -1,29 +1,31 @@
 module processor(
-	clk,
-	clk_en,
-	reset,
-	i1,	//MSB Instruction
-	i0,	//LSB Instruction,
-	c1,	//MSB state
-	c0	//LSB state
+	input clk,
+	input clk_en,
+	input reset,
+	input i1,	//MSB Instruction
+	input i0,	//LSB Instruction,
+	
+	output c1,	//MSB state
+	output c0	//LSB state
 );
-	input i1, i0;
-	output c1, c0;
-	wire i1, i0, c1, c0, d1, d0;
+
+	wire i1, i0, c1, c0, d1, d0 ,c0_,c1_;
 	assign d0 = ~c1 | c0&(~i1)&i0;
 	assign d1 = ~c1 | ~c0 | ~i0 | i1;
 	ff ff0 (
-		clk,
-		clk_en,
-		reset,
-		d0,
-		c0	
+		.clk   (clk),
+		.clk_en(clk_en),
+		.rst_n (reset),
+		.d3    (d0),
+		.q     (c0),
+		.q_    (c0_)
 	);
 	ff ff1 (
-		clk,
-		clk_en,
-		reset,
-		d1,
-		c1
+		.clk   (clk),
+		.clk_en(clk_en),
+		.rst_n (reset),
+		.d3    (d1),
+		.q     (c1),
+		.q_    (c1_)
 	);
 endmodule	
