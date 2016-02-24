778723a9a98bdbd3994a2b1dcef51efb955aece9
U-ZHENGQIXI\zheng_000 <zhengqixi@gmail.com>
2016-02-24 00:34:24 -0500

    Added 4-bit adder and demux

---
+++ adder.v
@@ -0,0 +1,15 @@
+module adder(
+	a1	, //MSB input 1
+	a0	, //LSB input 1
+	b1	, //MSB input 2
+	b0	, //LSB input 2
+	c1	, //MSB output
+	c0	  //LSB output
+);
+
+	input a1, a0, b1, b0;
+	output c1, c0;
+
+	assign c1 = (~a1)&(~a0)&(b1) | (~a1)&b1&(~b0) | a1&(~a0)&(~b1) | a1&(~b1)&(~b0) | (~a1)&a0&(~b1)&b0;
+	assign c0 = (~a0) & b0 | a0 & (~b0);
+endmodule
---
+++ demux.v
@@ -0,0 +1,16 @@
+module demux (in, //Single input 
+	c1, //MSB address of demux 
+	c2, //LSB address of demux 
+	inc, // (00) 
+	jnp, // (01) 
+	halt, //(10) 
+	start //(11)
+);
+	output inc, jnp, halt, start;
+	input in, c1, c2;
+	
+	assign inc = in & (~c1) & (~c2);
+	assign jnp = in & (~c1) & (c2);
+	assign halt = in & (c1) & (~c2);
+	assign start = in & (c1) & (c2);
+endmodule
