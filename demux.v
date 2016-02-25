61d67dee4a85a9553ce291617bf6ccf210a8c1a3
U-ZHENGQIXI\zheng_000 <zhengqixi@gmail.com>
2016-02-25 11:00:18 -0500

    it works, but doens't print stuff out

--- demux.v
+++ demux.v
@@ -1,10 +1,10 @@
 module demux (
 	c1, //MSB address of demux 
 	c0, //LSB address of demux 
-	inc, // (11) 
-	jnp, // (01) 
-	halt, //(10) 
-	start //(00)
+	inc, // (11) state
+	jnp, // (01) state
+	halt, //(10) state
+	start //(00) state
 );
 	output inc, jnp, halt, start;
 	input c1, c0;
@@ -12,7 +12,7 @@ module demux (
 	wire c1, c0, inc, jnp, halt, start;
 	
 	assign start = (~c1) & (~c0);
-	assign inc = (~c1) & (c0);
+	assign jnp = (~c1) & (c0);
 	assign halt = (c1) & (~c0);
-	assign jnp = (c1) & (c0);
+	assign inc = (c1) & (c0);
 endmodule
--- paperProcessor.v
+++ paperProcessor.v
@@ -8,25 +8,25 @@ module paperProcessor(
 	instruction
 );
 	input clk, clk_en, reset;
+	
 	output wire [2:0] registerCount;
 	output wire [1:0] programCount;
 	output wire [1:0] stateCount;
 	output wire [1:0] instruction;
-	wire c1, c0, halt, jnp, inc, start, r2, r1, r0, p1, p0, i1, i0;
+	
+	wire c1, c0, halt, jnp, inc, start, r2, r1, r0;
 	wire [1:0] address;
-	assign address = {p1, p0};
 	wire [1:0] data;
-	assign data = {i1, i0};
 	assign registerCount = {r2, r1, r0};
-	assign programCount = {p1, p0};
+	assign programCount = address;
 	assign stateCount = {c1, c0};
-	assign instruction = {i1, i0};
+	assign instruction = data;
 	processorCore alu (
 		.clk(clk),
 		.clk_en(clk_en),
 		.reset(reset),
-		.i1(i1),
-		.i0(i0),
+		.i1(data[1]),
+		.i0(data[0]),
 		.c1(c1),
 		.c0(c0)
 	);
@@ -50,10 +50,10 @@ module paperProcessor(
 		.jnp(jnp),
 		.inc(inc),
 		.r2(r2),
-		.i1(i1),
-		.i0(i0),
-		.p1(p1),
-		.p0(p0),
+		.i1(data[1]),
+		.i0(data[0]),
+		.p1(address[1]),
+		.p0(address[0]),
 		.clk(clk),
 		.rst_n(reset)
 	);
